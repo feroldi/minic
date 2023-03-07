fd5a2739a93d47ed402282b80e0f4af9ed07ea6f
Mario Feroldi <mferoldif@gmail.com>
2023-03-07 00:55:20 -0300

    feat: add IR generator

---
+++ minic/ir.py
@@ -0,0 +1,180 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+
+from minic import parser
+from minic.parser import (AssignStmt, BinOpExpr, NumberExpr, ParenExpr,
+                          PrintStmt, ProgramStmt, VarExpr)
+
+
+class AstVisitor:
+    def visit_program_stmt(self, program_stmt: ProgramStmt):
+        raise NotImplementedError()
+
+    def visit_assign_stmt(self, assign_stmt: AssignStmt):
+        raise NotImplementedError()
+
+    def visit_print_stmt(self, print_stmt: PrintStmt):
+        raise NotImplementedError()
+
+    def visit_paren_expr(self, paren_expr: ParenExpr):
+        raise NotImplementedError()
+
+    def visit_bin_op_expr(self, bin_op_expr: BinOpExpr):
+        raise NotImplementedError()
+
+    def visit_var_expr(self, var_expr: VarExpr):
+        raise NotImplementedError()
+
+    def visit_number_expr(self, number_expr: NumberExpr):
+        raise NotImplementedError()
+
+
+class IrGen(AstVisitor):
+    def __init__(self, program_ast):
+        self.program_ast = program_ast
+        self.reg_by_term = {}
+        self.reg_stack = []
+        self.reg_idx_counter = 0
+        self.instructions = []
+
+    def gen_program(self):
+        self.program_ast.accept(self)
+
+        return Program(
+            instructions=self.instructions,
+        )
+
+    def new_reg(self):
+        reg = Reg(self.reg_idx_counter)
+        self.reg_idx_counter += 1
+        return reg
+
+    def intern_term(self, term_key, reg):
+        self.reg_by_term[term_key] = reg
+
+    def get_interned_term(self, term_key):
+        assert self.is_term_interned(term_key)
+        return self.reg_by_term.get(term_key)
+
+    def is_term_interned(self, term_key):
+        return term_key in self.reg_by_term
+
+    def visit_program_stmt(self, program_stmt: ProgramStmt):
+        pass
+
+    def visit_assign_stmt(self, assign_stmt: AssignStmt):
+        out_reg = self.new_reg()
+        self.intern_term(assign_stmt.target_ident, out_reg)
+        in_reg = self.reg_stack.pop()
+        load_instr = LoadRegInstr(out_reg, in_reg)
+        self.instructions.append(load_instr)
+
+    def visit_print_stmt(self, print_stmt: PrintStmt):
+        arg_reg = self.reg_stack.pop()
+        print_instr = PrintInstr(arg_reg)
+        self.instructions.append(print_instr)
+
+    def visit_paren_expr(self, paren_expr: ParenExpr):
+        pass
+
+    def visit_bin_op_expr(self, bin_op_expr: BinOpExpr):
+        right_reg = self.reg_stack.pop()
+        left_reg = self.reg_stack.pop()
+
+        instr_op = bin_op_from_node_op(bin_op_expr.op)
+        term = (instr_op, right_reg, left_reg)
+
+        if self.is_term_interned(term):
+            out_reg = self.get_interned_term(term)
+        else:
+            out_reg = self.new_reg()
+            self.intern_term(term, out_reg)
+
+            bin_op_instr = BinOpInstr(
+                out_reg=out_reg,
+                op=instr_op,
+                left_reg=left_reg,
+                right_reg=right_reg,
+            )
+            self.instructions.append(bin_op_instr)
+
+        self.reg_stack.append(out_reg)
+
+    def visit_var_expr(self, var_expr: VarExpr):
+        var_reg = self.reg_by_term[var_expr.ident]
+        self.reg_stack.append(var_reg)
+
+    def visit_number_expr(self, number_expr: NumberExpr):
+        if self.is_term_interned(number_expr.val):
+            out_reg = self.get_interned_term(number_expr.val)
+        else:
+            out_reg = self.new_reg()
+            self.intern_term(number_expr.val, out_reg)
+
+            load_instr = LoadLiteralInstr(
+                out_reg=out_reg,
+                value=number_expr.val,
+            )
+            self.instructions.append(load_instr)
+
+        self.reg_stack.append(out_reg)
+
+
+class BinOp(Enum):
+    Add = auto()
+    Sub = auto()
+    Times = auto()
+    Div = auto()
+
+
+def bin_op_from_node_op(node_op: parser.BinOp) -> BinOp:
+    match node_op:
+        case parser.BinOp.Add:
+            return BinOp.Add
+        case parser.BinOp.Sub:
+            return BinOp.Sub
+        case parser.BinOp.Times:
+            return BinOp.Times
+        case parser.BinOp.Div:
+            return BinOp.Div
+
+    assert False
+
+
+@dataclass(frozen=True)
+class Reg:
+    idx: int
+
+
+class Instr:
+    pass
+
+
+@dataclass
+class Program:
+    instructions: list[Instr]
+
+
+@dataclass
+class LoadLiteralInstr(Instr):
+    out_reg: Reg
+    value: int
+
+
+@dataclass
+class LoadRegInstr(Instr):
+    out_reg: Reg
+    in_reg: Reg
+
+
+@dataclass
+class BinOpInstr(Instr):
+    out_reg: Reg
+    op: BinOp
+    left_reg: Reg
+    right_reg: Reg
+
+
+@dataclass
+class PrintInstr(Instr):
+    arg_reg: Reg
---
+++ tests/test_ir.py
@@ -0,0 +1,303 @@
+from minic.ir import (BinOp, BinOpInstr, IrGen, LoadLiteralInstr, LoadRegInstr,
+                      PrintInstr, Reg)
+from minic.parser import Parser
+from minic.scanner import Scanner
+
+
+def test_load_a_literal_into_a_register():
+    code = """
+    a = 42
+    b = 123
+    """
+
+    program = _ir_gen(code)
+
+    assert program.instructions == [
+        LoadLiteralInstr(out_reg=Reg(0), value=42),
+        LoadRegInstr(out_reg=Reg(1), in_reg=Reg(0)),
+        LoadLiteralInstr(out_reg=Reg(2), value=123),
+        LoadRegInstr(out_reg=Reg(3), in_reg=Reg(2)),
+    ]
+
+
+def test_load_a_register_into_another_register():
+    code = """
+    a = 42
+    b = a
+    """
+
+    program = _ir_gen(code)
+
+    assert program.instructions == [
+        LoadLiteralInstr(out_reg=Reg(0), value=42),
+        LoadRegInstr(out_reg=Reg(1), in_reg=Reg(0)),
+        LoadRegInstr(out_reg=Reg(2), in_reg=Reg(1)),
+    ]
+
+
+def test_simple_addition():
+    code = """
+    a = 15 + 25
+    """
+
+    program = _ir_gen(code)
+
+    assert program.instructions == [
+        LoadLiteralInstr(out_reg=Reg(0), value=15),
+        LoadLiteralInstr(out_reg=Reg(1), value=25),
+        BinOpInstr(out_reg=Reg(2), op=BinOp.Add, left_reg=Reg(0), right_reg=Reg(1)),
+        LoadRegInstr(out_reg=Reg(3), in_reg=Reg(2)),
+    ]
+
+
+def test_expression_of_operations():
+    code = """
+    a = 15
+    b = 20 + a * 52
+    """
+
+    program = _ir_gen(code)
+
+    assert program.instructions == [
+        # a = 15
+        LoadLiteralInstr(out_reg=Reg(0), value=15),
+        LoadRegInstr(out_reg=Reg(1), in_reg=Reg(0)),
+        # 20
+        LoadLiteralInstr(out_reg=Reg(2), value=20),
+        # 52
+        LoadLiteralInstr(out_reg=Reg(3), value=52),
+        # a * 52
+        BinOpInstr(
+            out_reg=Reg(4),
+            op=BinOp.Times,
+            left_reg=Reg(1),
+            right_reg=Reg(3),
+        ),
+        # 20 +
+        BinOpInstr(
+            out_reg=Reg(5),
+            op=BinOp.Add,
+            left_reg=Reg(2),
+            right_reg=Reg(4),
+        ),
+        # b =
+        LoadRegInstr(out_reg=Reg(6), in_reg=Reg(5)),
+    ]
+
+
+def test_expression_of_operations():
+    code = """
+    a = (20 + 15) * 52
+    """
+
+    program = _ir_gen(code)
+
+    assert program.instructions == [
+        # 20
+        LoadLiteralInstr(out_reg=Reg(0), value=20),
+        # 15
+        LoadLiteralInstr(out_reg=Reg(1), value=15),
+        # (20 + 15)
+        BinOpInstr(
+            out_reg=Reg(2),
+            op=BinOp.Add,
+            left_reg=Reg(0),
+            right_reg=Reg(1),
+        ),
+        # 52
+        LoadLiteralInstr(out_reg=Reg(3), value=52),
+        # * 52
+        BinOpInstr(
+            out_reg=Reg(4),
+            op=BinOp.Times,
+            left_reg=Reg(2),
+            right_reg=Reg(3),
+        ),
+        # a =
+        LoadRegInstr(out_reg=Reg(5), in_reg=Reg(4)),
+    ]
+
+
+def test_subtractions_and_divisions():
+    code = """
+    a = 15
+    b = 20 - a / 52
+    """
+
+    program = _ir_gen(code)
+
+    assert program.instructions == [
+        # a = 15
+        LoadLiteralInstr(out_reg=Reg(0), value=15),
+        LoadRegInstr(out_reg=Reg(1), in_reg=Reg(0)),
+        # 20
+        LoadLiteralInstr(out_reg=Reg(2), value=20),
+        # 52
+        LoadLiteralInstr(out_reg=Reg(3), value=52),
+        # a / 52
+        BinOpInstr(
+            out_reg=Reg(4),
+            op=BinOp.Div,
+            left_reg=Reg(1),
+            right_reg=Reg(3),
+        ),
+        # 20 -
+        BinOpInstr(
+            out_reg=Reg(5),
+            op=BinOp.Sub,
+            left_reg=Reg(2),
+            right_reg=Reg(4),
+        ),
+        # b =
+        LoadRegInstr(out_reg=Reg(6), in_reg=Reg(5)),
+    ]
+
+
+def test_print_a_value():
+    code = """
+    a = 42
+    print 0
+    print a
+    """
+
+    program = _ir_gen(code)
+
+    assert program.instructions == [
+        LoadLiteralInstr(out_reg=Reg(0), value=42),
+        LoadRegInstr(out_reg=Reg(1), in_reg=Reg(0)),
+        LoadLiteralInstr(out_reg=Reg(2), value=0),
+        PrintInstr(arg_reg=Reg(2)),
+        PrintInstr(arg_reg=Reg(1)),
+    ]
+
+
+def test_reuse_registers_when_the_same_number_is_found():
+    code = """
+    x = 42 + 42
+    print 42
+    """
+
+    program = _ir_gen(code)
+
+    assert program.instructions == [
+        LoadLiteralInstr(out_reg=Reg(0), value=42),
+        BinOpInstr(
+            out_reg=Reg(1),
+            op=BinOp.Add,
+            left_reg=Reg(0),
+            right_reg=Reg(0),
+        ),
+        LoadRegInstr(out_reg=Reg(2), in_reg=Reg(1)),
+        PrintInstr(arg_reg=Reg(0)),
+    ]
+
+
+def test_define_new_reg_when_shadowing_vars():
+    code = """
+    a = 42
+    a = 99
+    print a
+    """
+
+    program = _ir_gen(code)
+
+    assert program.instructions == [
+        LoadLiteralInstr(out_reg=Reg(0), value=42),
+        LoadRegInstr(out_reg=Reg(1), in_reg=Reg(0)),
+        LoadLiteralInstr(out_reg=Reg(2), value=99),
+        LoadRegInstr(out_reg=Reg(3), in_reg=Reg(2)),
+        PrintInstr(arg_reg=Reg(3)),
+    ]
+
+
+def test_eliminate_common_subexpressions():
+    code = """
+    a = (1 + 2) * (1 + 2)
+    print a
+    """
+
+    program = _ir_gen(code)
+
+    assert program.instructions == [
+        LoadLiteralInstr(out_reg=Reg(0), value=1),
+        LoadLiteralInstr(out_reg=Reg(1), value=2),
+        BinOpInstr(
+            out_reg=Reg(2),
+            op=BinOp.Add,
+            left_reg=Reg(0),
+            right_reg=Reg(1),
+        ),
+        BinOpInstr(
+            out_reg=Reg(3),
+            op=BinOp.Times,
+            left_reg=Reg(2),
+            right_reg=Reg(2),
+        ),
+        LoadRegInstr(out_reg=Reg(4), in_reg=Reg(3)),
+        PrintInstr(arg_reg=Reg(4)),
+    ]
+
+
+def test_do_not_eliminate_common_subexpressions_when_var_changes():
+    code = """
+    a = 42
+    b = (a + 2) * (a + 2)
+    a = 43
+    c = (a + 2) * (a + 2)
+    print c
+    """
+
+    program = _ir_gen(code)
+
+    assert program.instructions == [
+        # a = 42
+        LoadLiteralInstr(out_reg=Reg(0), value=42),
+        LoadRegInstr(out_reg=Reg(1), in_reg=Reg(0)),
+        # (a + 2)
+        LoadLiteralInstr(out_reg=Reg(2), value=2),
+        BinOpInstr(
+            out_reg=Reg(3),
+            op=BinOp.Add,
+            left_reg=Reg(1),
+            right_reg=Reg(2),
+        ),
+        # b = (a + 2) * (a + 2)
+        BinOpInstr(
+            out_reg=Reg(4),
+            op=BinOp.Times,
+            left_reg=Reg(3),
+            right_reg=Reg(3),
+        ),
+        LoadRegInstr(out_reg=Reg(5), in_reg=Reg(4)),
+        # a = 43
+        LoadLiteralInstr(out_reg=Reg(6), value=43),
+        LoadRegInstr(out_reg=Reg(7), in_reg=Reg(6)),
+        # (a + 2)
+        BinOpInstr(
+            out_reg=Reg(8),
+            op=BinOp.Add,
+            left_reg=Reg(7),
+            right_reg=Reg(2),
+        ),
+        # c = (a + 2) * (a + 2)
+        BinOpInstr(
+            out_reg=Reg(9),
+            op=BinOp.Times,
+            left_reg=Reg(8),
+            right_reg=Reg(8),
+        ),
+        LoadRegInstr(out_reg=Reg(10), in_reg=Reg(9)),
+        # print c
+        PrintInstr(arg_reg=Reg(10)),
+    ]
+
+
+# TODO: Test whether reassigning a var and doing the same expression makes it incorrectly eliminate.
+
+
+def _ir_gen(code: str):
+    scanner = Scanner(code)
+    parser = Parser(scanner=scanner)
+    ir_gen = IrGen(parser.parse_program())
+
+    return ir_gen.gen_program()
